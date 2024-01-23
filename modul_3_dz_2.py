340d34500d55f6d7a3f909b09c11b7970d39e3bf
@YehorChykin <mebel4268@gmail.com>
2024-01-23 18:12:05 +0100

    Dz_3_2_Done

---
+++ modul_3_dz_2.py
@@ -0,0 +1,14 @@
+# six number in (1,49)
+# five number in (1,36)
+import random
+def get_numbers_ticket(min, max, quantity):
+    if (1 <= min <= max <= 1000) and min<=quantity<=max:
+        all_numbers = list(range(min, max+1))
+        lottery_numbers=random.sample(all_numbers, quantity)
+        return lottery_numbers
+    else:
+        print(f"Введіть корректні дані")
+        return []
+
+lottery_numbers = get_numbers_ticket(1, 49, 50)
+print("Ваші лотерейні числа:", lottery_numbers)
